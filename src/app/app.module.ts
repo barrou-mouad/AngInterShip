816f10a77d8504b0c3086d46612ebac5ae847ad3
BARROU Mouad <81191265+barrou-mouad@users.noreply.github.com>
2022-04-14 02:57:34 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { SidenavbarComponent } from './sidenavbar/sidenavbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    FooterComponent,
+    HeaderComponent,
+    SidenavbarComponent
   ],
   imports: [
     BrowserModule,
